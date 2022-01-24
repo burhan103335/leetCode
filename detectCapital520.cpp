2a50f42ea5f1b1a08f1577e1d170fe761165969b
Burhan uddin <burhan01733700745@gmail.com>
2022-01-24 01:27:09 +0000

    Add 24 jan 2022 daily challenge

---
+++ detectCapital520.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+
+        int n = word.length(), upper = 0;
+        
+        for(int i = 0; i<n; i++)
+            if(word[i]>='A' && word[i]<='Z') upper++;
+        
+        if(upper==n || upper == 0) return true;
+        else if(upper==1 && (word[0]>='A' && word[0]<='Z')) return true;
+        else return false;
+    }
+};
