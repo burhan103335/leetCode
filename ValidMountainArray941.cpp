a56dbff897ceceb011f8d178b7d615f2cdf50b6c
Burhan uddin <burhan01733700745@gmail.com>
2022-01-25 01:27:37 +0000

    last commit design change

--- ValidMountainArray941.cpp
+++ ValidMountainArray941.cpp
@@ -3,9 +3,11 @@ public:
     bool validMountainArray(vector<int>& arr) {
         
         int n = arr.size(), i = 0;
-        
+
         while(i<n-1 && arr[i]<arr[i+1]) i++;
+        
         if(i==0 || i==n-1) return 0;
+        
         while(i<n-1 && arr[i]>arr[i+1]) i++;
         
         return i==n-1 ;
