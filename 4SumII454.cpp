9c18347bdb01bbfafbf0086bd4137714e82b9138
Burhan uddin <burhan01733700745@gmail.com>
2022-02-03 12:10:05 +0000

    454. 4Sum II Leetcode problem solving 03 feb 2022 Another version

--- 4SumII454.cpp
+++ 4SumII454.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        int cnt = 0, sum;
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int n = nums1.size(), cnt = 0, sum;
         unordered_map<int, int>mp;
         
-        for(int a : A)
-            for(int b : B)
-                ++mp[a + b];
+        for(int i = 0; i<n; i++)
+            for(int j = 0; j<n; j++)
+                mp[nums4[i]+ nums3[j]]++;
         
-        for(int c : C){
-            for(int d : D){
-                sum = 0 - (c + d);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                sum = 0 - (nums1[i]+ nums2[j]);
                 cnt+=mp[sum];
             }
         }
