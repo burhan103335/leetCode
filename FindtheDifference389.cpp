23d96d006200842db35a5663e983364db812811a
Burhan <burhan.neub@gmail.com>
2022-02-07 12:48:34 +0000

    Find the Difference 389 Leetcode solve 07 Feb 2022

--- FindtheDifference389.cpp
+++ FindtheDifference389.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        
         int n = s.length(), sum = 0;
-        
+
         for(int i = 0; i<n; i++) sum+= (t[i] - s[i]);
-        
+
         return sum + t[n];
-        
     }
 };
