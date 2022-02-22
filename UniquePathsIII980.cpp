9d64ea2d8847e79dae2d2976b1dc1b66c46a821c
Burhan <burhan01733700745@gmail.com>
2022-02-22 04:17:18 +0000

    980. Unique Paths III Update

--- UniquePathsIII980.cpp
+++ UniquePathsIII980.cpp
@@ -1,10 +1,11 @@
 class Solution {
     
-    void travers(vector<vector<int>> v, int i, int j, int em){
+    int sum, m, n, st, ed, em;
+    
+    void travers(vector<vector<int>>& v, int i, int j, int em){
         
         if(i<0 || j<0 || i>=n || j>= m || v[i][j] == -1 || v[i][j] == 1) return;
         
-        //cout<<i<<" "<<j<<" "<<em<<endl;
         
         if(v[i][j]==2) {
             if(em==0) sum ++;
@@ -13,22 +14,23 @@ class Solution {
         else {
             em--;
             v[i][j] = 1;
+            
             travers(v, i+1, j, em);
             travers(v, i-1, j, em);
             travers(v, i, j+1, em);
             travers(v, i, j-1, em);
+            
+            v[i][j] = 0;
         }
         
     }
     
-    
-    int sum, m, n, pi, pj, st, ed, em;
 public:
     int uniquePathsIII(vector<vector<int>>& v) {
+        
         sum = 0, em = 0;
         
-        n = v.size();
-        m = v[0].size();
+        n = v.size(), m = v[0].size();
         
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<m; j++) {
@@ -36,13 +38,11 @@ public:
                     st = i, ed = j;
                     v[i][j] = 0;
                 }
-                else if(v[i][j]==2){
-                    pi = i, pj = j;
-                }
+                
                 else if(v[i][j]==0) em++;
             }
         }
-        //cout<<em<<" "<<st<<" "<<ed<<" "<<pi<<" "<<pj;
+        
         travers(v, st, ed, em+1);
         return sum;
     }
