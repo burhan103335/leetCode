91f72e7ebd12c9b49d69402271fffbe9eda979e5
Burhan <burhan01733700745@gmail.com>
2022-02-23 03:40:45 +0000

    133. Clone Graph Leetcode problem solve 23 Feb 2022 update with BFS

--- CloneGraph133.cpp
+++ CloneGraph133.cpp
@@ -1,26 +1,32 @@
-unordered_map<int, Node*> mp;
-
-Node* dfs(Node* node) {
-    if(node == nullptr) return nullptr;
-    
-    mp[node->val] = new Node(node->val);
-    int n = node->neighbors.size();
-    for(int i = 0; i<n; i++) {
-        if(mp[node->neighbors[i]->val]){
-            mp[node->val]->neighbors.push_back(mp[node->neighbors[i]->val]);
-        }
-        else mp[node->val]->neighbors.push_back(dfs(node->neighbors[i]));
-    }
-    return mp[node->val];
-    
-}
-
-
-
+queue<Node*>q;
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        mp.clear();
-        return dfs(node);
+   
+        if(node == nullptr) return nullptr;
+        unordered_map<int, Node*> mp;
+        
+        mp[1] = new Node(node->val);
+        
+        q.push(node);
+        
+        while(!q.empty()) {
+            node = q.front();
+            q.pop();
+            
+            for(auto n : node->neighbors) {
+                if(!mp[n->val]) {
+                    q.push(n);
+                    mp[n->val] = new Node(n->val);
+                }
+                
+                mp[node->val]->neighbors.push_back(mp[n->val]);
+            }
+            
+        }
+        
+        
+        return mp[1];
+        
     }
 };
