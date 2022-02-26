5a04a389eb63c8f65e13a378266a5a1b9b927f9a
Burhan <burhan01733700745@gmail.com>
2022-02-26 03:16:03 +0000

    350 Intersection of Two Arrays II leetcode problem solve 26 Feb 2022 update with o(n^2)

--- 350IntersectionofTwoArraysII.cpp
+++ 350IntersectionofTwoArraysII.cpp
@@ -2,15 +2,18 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int>mp;
-        vector<int>v;
-        for(int num : nums1) mp[num+1]++;
-        for(int num : nums2) {
-            if(mp[num+1]) {
-                v.push_back(num);
-                mp[num+1]--;
+        vector<int> v;
+        
+        for(int i = 0; i<nums1.size(); i++){
+            for(int j = 0; j<nums2.size(); j++) {
+                if(nums1[i]==nums2[j]) {
+                    nums2[j] = -1;
+                    v.push_back(nums1[i]);
+                    break;
+                }
             }
         }
+        
         return v;
     }
 };
