43c347c8ace9aeeefdf87377661e496db56f8175
Burhan <burhan01733700745@gmail.com>
2022-02-26 03:20:31 +0000

    350 Intersection of Two Arrays II leetcode problem solve 26 Feb 2022 update with o(nlogn) with sort first

--- 350IntersectionofTwoArraysII.cpp
+++ 350IntersectionofTwoArraysII.cpp
@@ -4,14 +4,18 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> v;
         
-        for(int i = 0; i<nums1.size(); i++){
-            for(int j = 0; j<nums2.size(); j++) {
-                if(nums1[i]==nums2[j]) {
-                    nums2[j] = -1;
-                    v.push_back(nums1[i]);
-                    break;
-                }
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int i = 0, j = 0, n1 = nums1.size(), n2 = nums2.size();
+        
+        while(i<n1 && j<n2) {
+            if(nums1[i] == nums2[j]){
+                v.push_back(nums1[i]);
+                i++, j++;
             }
+            else if(nums1[i] < nums2[j]) i++;
+            else j++;
         }
         
         return v;
