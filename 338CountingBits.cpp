aa17d315289ae7b789ec83ae87b00df947f442f1
Burhan <burhan01733700745@gmail.com>
2022-03-02 00:26:22 +0000

    338. Counting Bits leetcode problem solving 01 Feb 2022 update

--- 338CountingBits.cpp
+++ 338CountingBits.cpp
@@ -1,19 +1,9 @@
 //338. Counting Bits
-class Solution {
-    int cnt(int i) {
-        int ct = 0;
-        while(i){
-            ct+=(i%2);
-            i/=2;
-        }
-        return ct;
-    }
-    
+class Solution {    
 public:
     vector<int> countBits(int n) {
-        vector<int>v;
-        for(int i = 0; i<=n; i++)
-            v.push_back(cnt(i));
+        vector<int>v(n+1);
+        for(int i = 1; i<=n; i++) v[i] = v[i/2] + i%2;
         return v;
     }
 };
